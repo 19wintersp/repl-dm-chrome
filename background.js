3e5bceca0819cd6c29db72e73ad1149f58a47841
Patrick Winters <61561933+19wintersp@users.noreply.github.com>
2021-03-21 19:20:57 +0000

    Promise-based script injection

--- background.js
+++ background.js
@@ -4,17 +4,24 @@ chrome.tabs.onActivated.addListener(tab => {
     addScripts(tab);
 })
 
+function injectFile(file) {
+    return new Promise((resolve) => {
+        chrome.tabs.executeScript(null, { file }, () => resolve());
+    });
+}
+
 function addScripts(tab) {
-    chrome.tabs.get(tab.tabId, currentTabInfo => {
+    chrome.tabs.get(tab.tabId, async currentTabInfo => {
         if (/^https:\/\/repl\.it/.test(currentTabInfo.url)) {
             chrome.tabs.insertCSS(null, {file: './repldm-page.css'});
-            chrome.tabs.executeScript(null, {file: './jquery.min.js'}, () => {
-                chrome.tabs.executeScript(null, {file: './sanitize-html.js'}, () => {
-                    chrome.tabs.executeScript(null, {file: './marked.min.js'}, () => {
-                        chrome.tabs.executeScript(null, {file: './foreground.js'}, () => { console.log('i injected') })
-                    })
-                })
-            })
+
+            await Promise.all([
+                injectFile("./jquery.min.js"),
+                injectFile("./sanitize-html.js"),
+                injectFile("./marked.min.js")
+            ]);
+            await injectFile("./foreground.js");
+            console.log("i injected");
         }
     })
 }
